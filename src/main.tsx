a11cb67257c9442478da70f50fa74beedc7ca2f8
Jamboxman5 <samthompson1818@gmail.com>
2025-09-14 22:14:00 -0400

    Switched browserrouter to hashrouter for pages deployment

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom';
-import App from './App.tsx'
-import { AuthProvider } from './services/AuthProvider.tsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { HashRouter } from "react-router-dom";
+import App from "./App.tsx";
+import { AuthProvider } from "./services/AuthProvider.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
-    </BrowserRouter>
-  </StrictMode>,
-)
+    </HashRouter>
+  </StrictMode>
+);
